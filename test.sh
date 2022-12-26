f191e100c973acdb8993ccb3b39ca9d2ddb4ac86
shuangxiang kan <18550887212@163.com>
2022-12-26 12:10:45 +1100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-result="Detected 0 taint flow(s):"
-resultTxt=./result.txt
+result="Detected 0 taint flow(s):";
+resultTxt=./result.txt;
 
  if [ ! -f $resultTxt ];then
-    echo "result.txt does not exist"
+    echo "result.txt does not exist";
     printf '\033[1;31;40m[fail]%b\033[0m\n';
+    exit 1;
 
   else
-    passresult=$(grep "$result" $resultTxt)
+    passresult=$(grep "$result" $resultTxt);
     if [ -n "$passresult" ];then
-        echo "result is wrong"
+        echo "result is wrong";
         printf '\033[1;31;40m[fail]%b\033[0m\n';
+        exit 1;
     else
         printf '\033[1;32;40m[success]%b\033[0m\n';
     fi
